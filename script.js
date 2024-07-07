b3761defa60cfc9863883e620ece6f6d306cc8dd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-07 20:48:30 +0530

    animated nav links

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const scroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true,
 });
+
+gsap.from(".nav-link", {
+  stagger: .2,
+  y: 20,
+  duration: 1,
+  ease: "power2",
+  opacity: 0,
+});
