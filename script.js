b94657aca5631b007ed225f819b6f6d45cfe6d21
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-07 21:10:21 +0530

    added cdn

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const scroll = new LocomotiveScroll({
 });
 
 gsap.from(".nav-link", {
-  stagger: .2,
+  stagger: 0.2,
   y: 20,
   duration: 1,
   ease: "power2",
   opacity: 0,
+});
+
+Shery.textAnimate("#headings h1", {
+  style: 2,
+  y: 10,
+  delay: 0.1,
+  duration: 2,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  multiplier: 0.1,
 });
